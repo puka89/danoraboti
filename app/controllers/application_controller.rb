5bf8ecc86cbb2adab6c0ada7be8edd335fc5b2ba
puka89 <ludia.kud@abv.bg>
2017-10-11 00:39:08 +0300

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ require 'linear-regression'
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_sessions
   skip_before_action :verify_authenticity_token
+  
   def sums
     CSV.foreach do |row|
     sum=0
@@ -10,6 +11,7 @@ class ApplicationController < ActionController::Base
     sum=sum.ceil
     render plain: ('%.2f'%sum)  
   end  
+    
   def filters
     file=CSV.parse(params[:file].read)
     sum=0
@@ -21,6 +23,7 @@ class ApplicationController < ActionController::Base
     sum=sum.ceil
     render plain: ('%.2f'%sum)
   end 
+    
   def intervals
     sum=0
     red=1
@@ -41,16 +44,5 @@ class ApplicationController < ActionController::Base
    bestDay=bestDay.ceil
     render plain: ('%.2f' % bestDay)
   end
-
-  def lin_regressions
-    file=CSV.parse(params[:file].read)
-    ind=(1..file.length).to_a;
-    values=file.map{|n| n[0].to_i}
-    p values.length
-    p ind.length
-    linear = Regression::Linear.new(ind, values)
-    b = linear.intercept
-    a = linear.slope
-    render plain: "#{'%.6f' % a},#{'%.6f' % b}"
-  end  
+    
 end
